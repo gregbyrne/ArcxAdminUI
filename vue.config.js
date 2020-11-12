e3db9a5747806f5439d529cfbbd47e40a7c87a2b
jpowell <powell.joshua@epa.gov>
2020-11-12 16:47:34 -0500

    Adding new title and icon for browser tab.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   devServer: {
     port: 8081
+  },
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = 'ARC-X Admin'
+          return args
+        })
   }
 }
