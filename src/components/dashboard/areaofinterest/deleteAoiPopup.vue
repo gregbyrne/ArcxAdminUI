bd6763affdace0770aab426cfe252dc32f215f59
greg19735 <greg19735@gmail.com>
2020-12-07 15:35:04 -0500

    Add icons.

---
+++ src/components/dashboard/areaofinterest/deleteAoiPopup.vue
@@ -0,0 +1,127 @@
+<template>
+    <v-row justify="center">
+
+        <v-dialog
+                v-model="dialog"
+                persistent
+                max-width="600px"
+        >
+            <template v-slot:activator="{ on, attrs }" >
+                <v-btn
+                        color="primary"
+                        dark
+                        small
+                        v-bind="attrs"
+                        v-on="on"
+                >
+                    delete
+                </v-btn>
+            </template>
+            <v-card>
+                <v-card-title>
+                    <span class="headline">Delete Area of Interest</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field
+                                        v-model="areaname"
+                                        label="Area Of Interest to delete"
+                                        required
+                                        readonly
+                                ></v-text-field>
+                            </v-col>
+
+
+                        </v-row>
+                    </v-container>
+                    Are you sure you want to delete {{areaname}}? This will also delete all items and sub-items associated with the area.
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                            color="blue darken-1"
+                            text
+                            @click="dialog = false"
+                    >
+                        Close
+                    </v-btn>
+                    <v-btn
+                            color="blue darken-1"
+                            text
+                            @click="deleteAreaOfInterest(areaid)"
+                    >
+
+
+                        Delete
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+
+
+</template>
+
+<script>
+    import axios from "axios";
+    const API_URL = process.env.VUE_APP_API_URL;
+    const AOE_URL = process.env.VUE_APP_API_AREA_OF_INTEREST_URL;
+
+
+    export default {
+        name: 'Popup',
+        aoeName: 'default',
+        props: ['areaname', 'areaid'],
+
+
+        data: () => ({
+
+            dialog: false,
+            notifications: false,
+            sound: true,
+            widgets: false,
+
+        }),
+        methods:{
+
+            //DELETE
+            deleteAreaOfInterest(areaid)
+            {
+                alert("delete started id =  " + areaid)
+
+                const headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + this.$store.state.auth.user.accessToken
+                }
+                alert(API_URL + AOE_URL + areaid);
+                alert(headers);
+
+                axios.delete(API_URL + AOE_URL + areaid,{ 'headers': headers})
+                    .then(function (response) {
+                        if (response.status == 204) {
+                            alert(response.status)
+                            alert('Area of Interest has been deleted');
+                        }
+                        else
+                        {
+                            alert('Area of Interest was not deleted');
+                        }
+                    })
+                    .catch((error) => {
+                        alert('ERROR: with delete ' + error);
+                    });
+
+                this.dialog = false;
+
+
+
+            },
+
+
+
+        }
+
+    }
+</script>
